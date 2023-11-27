e4937b2b5371466f63af2f63019568388bb1984c
Magnus Siegumfeldt <m.ks@outlook.dk>
2023-11-27 12:16:58 +0100

    Binary dump

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,22 @@
 
 #include "simulator.h"
 
-#define NO_ERR 0;
-
 int main(void) {
     std::ifstream binary("../test/test_cases/task1/AddLarge.bin", std::ios::in | std::ios::binary);
     if(!binary) {
         std::cout << "Cannot open file!" << std::endl;
         return 1;
     }
-    Simulator::run(binary, false);
-    return NO_ERR;
+    
+    std::array<uint32_t, 32> reg = Simulator::run(binary, false);
+    
+    std::ofstream res("../out.res", std::ios::out | std::ios::binary);
+    if(!res) {
+      std::cout << "Cannot open file!" << std::endl;
+      return 1;
+    }
+    for (int i = 0; i < reg.size(); i++) {
+        res.write((char *) &reg[i], sizeof(reg[i]));
+    }
+    return 0;
 }
